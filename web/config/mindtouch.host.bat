aeb2ed37eb2d29ff4cdbb6b6408fee2fd43da309
Heran <qianwen1038@yahoo.com>
2013-08-12 14:29:53 +0800

    refresh

--- web/config/mindtouch.host.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-set CMD="D:\workspace\DekiWiki-Head\web\bin\mindtouch.host.exe" apikey dX3UOi5ZWqf3Pixsj0TlvLZl4aZsvrIm script C:\dekiwiki\mindtouch.deki.startup.xml path-prefix @api http-port 8081 ip localhost notty
-%CMD%
